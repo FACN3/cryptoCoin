7a48317c0a28ca1cd8ee7cf99cdcae87d15ac8d6
ionush <neilasjoseph@gmail.com>
2018-01-14 13:00:56 +0200

    setting up webpack relates to issue #1 wrote basic .babelrc, .eslintrc.json, webpack.config.js files and installed init modules

---
+++ js/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div className ='test'><h1>hello superstars</h1></div>
+);
+
+
+render(<App />, document.getElementById('root'));
