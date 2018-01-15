0032a106095c92592433da06a5aac884af86f753
MarlenAwwad <marlenawsh3@gmail.com>
2018-01-15 16:52:47 +0200

    Adding the navbar relates #7 we set the navbars, the css and routing to the other pages.

--- js/App.jsx
+++ js/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Landing from './Landing';
-import Learn from './Learn';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Landing from "./Landing";
+import Learn from "./Learn";
+import Markets from "./Markets";
+import Buy from "./Buy";
 
 const App = () => (
   <div>
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Landing} />
-        <Route path='/learn' component={Learn} />
+        <Route exact path="/" component={Landing} />
+        <Route path="/learn" component={Learn} />
+        <Route path="/markets" component={Markets} />
+        <Route path="/buy" component={Buy} />
       </Switch>
     </BrowserRouter>
   </div>
