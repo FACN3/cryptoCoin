3f5caba7046f65268aae49125b70cc0902e5bddf
ionush <neilasjoseph@gmail.com>
2018-01-16 10:53:51 +0200

    updated changes to styling relates #4 added backup fonts, changed button styling to global css page

---
+++ src/js/Learn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Learn = () => (
+  <div>
+    <Navbar page="learn" />
+    <h1>Learn PAGE</h1>
+  </div>
+);
+
+export default Learn;
